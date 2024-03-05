6bf23a0112d2ee368acb6e2e9fe5c3bfb8ba1555
navjotsidhu <sidhunavjot001@gmail.com>
2024-03-05 22:32:26 +0530

    find Factorial of a Number  using various ways

---
+++ Python If Else.py
@@ -0,0 +1,34 @@
+"""
+Given an integer, n , perform the following conditional actions:
+If  n is odd, print Weird
+If  n is even and in the inclusive range of  to , print Not Weird
+If  n is even and in the inclusive range of  to , print Weird
+If  n is even and greater than , print Not Weird
+"""
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Function to perform conditional actions
+def check_number(n):
+    if n % 2 != 0:
+        print("Weird")
+    elif n % 2 == 0 and 2 < n < 5:
+        print("Not Weird")
+    elif n % 2 == 0 and 6 <= n <= 20:
+        print("Weird")
+    elif n % 2 == 0 and n > 20:
+        print("Not Weird")
+
+
+# Main function
+if __name__ == '__main__':
+    # Reading input integer
+    n = int(input("Enter number: ").strip())
+
+    # Checking the number
+    check_number(n)
