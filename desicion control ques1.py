be63f3fec47dbe44be704251a02de6034add852a
navjotsidhu <sidhunavjot001@gmail.com>
2024-03-08 13:46:38 +0530

    find Factorial of a Number  using various ways

---
+++ desicion control ques1.py
@@ -0,0 +1,10 @@
+"""Check whether a given number is positiove or non positive"""
+
+a = int(input("Enter your number"))
+
+if a>0:
+    print("Positive Number")
+elif a ==0:
+    print("Its a ZERO")
+else:
+    print("Negative number")
