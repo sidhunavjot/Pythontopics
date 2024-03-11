f0970076b692e67038f3bad816e4164793665a32
navjotsidhu <sidhunavjot001@gmail.com>
2024-03-11 20:35:33 +0530

    find Factorial of a Number  using various ways

--- DQ7.py
+++ DQ7.py
@@ -1,7 +1,7 @@
 """Write a python script to check whether a given number is a three digit
 number
 or not"""
-
+#question update
 def threedigit(num):
     return 100 <= num <=999
 
@@ -12,5 +12,6 @@ if __name__ =='__main__':
         print("3 digit num")
     else:
         print("Not a 3 digit")
+
 
 
